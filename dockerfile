d83fd5b2f2c8f65d21294421f9de4b74aca4286c
鄭采妮 <184071@cch.org.tw>
2024-06-03 09:37:13 +0800

    test

---
+++ dockerfile
@@ -0,0 +1,11 @@
+# Base image
+FROM node:latest
+# Create app directory
+WORKDIR /usr/src/app
+# Install app dependencies
+COPY package.json /usr/src/app/
+RUN yarn install && yarn cache clean
+# Bundle app source
+COPY . /usr/src/app
+EXPOSE 8080
+CMD [ "yarn", "start" ]
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios=require('axios');
+const app = express();
+
+app.get('/getAllStock', (req, res) => {
+  axios.get("https://openapi.twse.com.tw/v1/exchangeReport/TWTB4U")
+      .then((response) => {
+        res.send(response.data);
+        //console.log(response.data)
+       /* setAllStock(response.data);
+        switch (selectKind) {
+          case "stockCode":
+            var arr = response.data.filter((d) => d.Code == keyword);
+            setAllStock(arr);
+            break;
+
+          case "stockName":
+            var arr = response.data.filter((d) => d.Name.includes(keyword));
+            setAllStock(arr);
+            break;
+        }*/
+      })
+      .catch((error) => console.log(error));
+})
+
+app.get('/bye', (req, res) => {
+  res.send('bye!')
+})
+
+app.listen('/api')
