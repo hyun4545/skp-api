f09fa86912df843293d90fbb89bf76a5c81e133b
鄭采妮 <184071@cch.org.tw>
2024-06-03 15:26:19 +0800

    add insert mongodb

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const axios=require('axios');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 // Use CORS middleware
 app.use(cors());
+
+const uri = "mongodb+srv://amy:amy0917@cluster0.yewt8nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.post('/saveUserStockes', async (req, res) => {
+  try {
+    await client.connect();
+    await client.db("skp")
+          .collection("skp-user-stockes")
+          .insertMany(req.body);
+  } finally {
+    await client.close();
+  }
+});
 
 app.get('/getAllStock', (req, res) => {
   axios.get("https://openapi.twse.com.tw/v1/exchangeReport/TWTB4U")
